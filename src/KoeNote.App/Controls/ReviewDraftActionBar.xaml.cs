e01cdafc6cc41aa6a21daa642d7d4c1d4d733047
Tomoaki Kawada <TommyKammy@users.noreply.github.com>
2026-06-11 12:44:43 +0900

    Split review draft panel controls

---
+++ src/KoeNote.App/Controls/ReviewDraftActionBar.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace KoeNote.App.Controls;
+
+public partial class ReviewDraftActionBar : UserControl
+{
+    public ReviewDraftActionBar()
+    {
+        InitializeComponent();
+    }
+}
